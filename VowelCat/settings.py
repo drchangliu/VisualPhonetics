70a2e6be1277a991dc4f1fb2561a8fd70910f297
Elijah <er693310@ohio.edu>
2015-01-29 16:59:55 -0500

    Adding lines to settings.py. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -103,14 +103,7 @@ STATICFILES_DIRS = (
    os.path.join(os.path.dirname(BASE_DIR), "VisualPhonetics", "static"),
 )
 
-"""
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-"""
+if not os.environ.get("HOME") == '/Users/ElijahRippeth':
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
