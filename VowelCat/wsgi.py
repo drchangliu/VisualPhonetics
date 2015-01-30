c5322f3e8d2761677364929ec1701760c04982d5
Elijah <er693310@ohio.edu>
2015-01-29 23:51:21 -0500

    Fixing wsgi. erip

--- VowelCat/wsgi.py
+++ VowelCat/wsgi.py
@@ -5,6 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VowelCat.settings")
@@ -14,7 +15,6 @@ application = get_wsgi_application()
 
 try:
     from dj_static import Cling
-
     application = Cling(get_wsgi_application())
 except:
     pass
