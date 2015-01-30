7f1ee5b32bde7d8b6ad879cbf3fe5e803fc41a86
Elijah <er693310@ohio.edu>
2015-01-29 23:47:24 -0500

    Adding STATIC_ROOT to project. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -14,9 +14,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-from path import Path
-PROJECT_ROOT = Path(__file__).abspath().dirname().dirname()
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -87,23 +84,24 @@ AUTH_PROFILE_MODULE = "account.UserProfile"
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "VisualPhonetics", "static")
+
 # Template location
 
-"""
 TEMPLATE_DIRS = (
    os.path.join(os.path.dirname(BASE_DIR), "VisualPhonetics", "static", "templates"),
-)
-"""
-
-TEMPLATE_DIRS = (
-    PROJECT_ROOT / 'templates',
 )
 
 STATICFILES_DIRS = (
    os.path.join(os.path.dirname(BASE_DIR), "VisualPhonetics", "static"),
 )
 
-if not os.environ.get("HOME") == '/Users/ElijahRippeth':
-    # Parse database configuration from $DATABASE_URL
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config()
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
