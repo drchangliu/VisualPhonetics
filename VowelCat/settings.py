a71a3a9e79297b594320eb0c98f8bc584a335bc3
Elijah <er693310@ohio.edu>
2015-02-03 16:55:40 -0500

    Fixing path. I hope this works. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -91,11 +91,11 @@ MEDIA_ROOT = 'media'
 # Template location
 
 TEMPLATE_DIRS = (
-   os.path.join(os.path.dirname(BASE_DIR), "VisualPhonetics", "VowelCat", "static", "templates"),
+   os.path.join(os.path.dirname(BASE_DIR), "templates"),
 )
 
 STATICFILES_DIRS = (
-   os.path.join(os.path.dirname(BASE_DIR), "VisualPhonetics", "VowelCat", "static"),
+   os.path.join(os.path.dirname(BASE_DIR), "static"),
 )
 """
 # Parse database configuration from $DATABASE_URL
