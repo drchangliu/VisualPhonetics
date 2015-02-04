2fbdb563669dc658cdea4dd2c9a27ebbed2e96e9
Elijah <er693310@ohio.edu>
2015-02-04 16:15:39 -0500

    Moving static. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -98,7 +98,7 @@ STATICFILES_DIRS = (
    os.path.join(os.path.dirname(BASE_DIR), "static"),
 )
 
-
+"""
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
@@ -108,3 +108,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+"""
