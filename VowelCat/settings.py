3c5cc8de0d94f85ee64525dbcb5bcf78894aa26d
Elijah <er693310@ohio.edu>
2015-02-05 16:55:23 -0500

    Fixing settings.py. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -23,8 +23,6 @@ DEBUG = True
 #DEBUG = False
 
 TEMPLATE_DEBUG = True
-
-
 
 ALLOWED_HOSTS = []
 
