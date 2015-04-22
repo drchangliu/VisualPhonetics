daaa83973797cf4450f0f5a5e86452bf994652e8
Elijah <er693310@ohio.edu>
2015-04-22 17:05:04 -0400

    Removing VowelCat2.0 from this branch. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -101,22 +101,20 @@ STATICFILES_DIRS = (
    os.path.join(os.path.dirname(BASE_DIR), "VowelCat", "static"),
 )
 
-try:
-  # Parse database configuration from $DATABASE_URL
-  import dj_database_url
-  DATABASES['default'] =  dj_database_url.config()
 
-  # Enable Connection Pooling
-  DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-  # Allow all host headers
-  ALLOWED_HOSTS = ['*']
+# Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
-  # Simplified static file serving.
-  # https://warehouse.python.org/project/whitenoise/
-  STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-except:
-  pass
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
