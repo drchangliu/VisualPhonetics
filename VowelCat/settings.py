2f1fc8fbc6ab52e189f226b32c5e57c93e16bdda
Elijah <er693310@ohio.edu>
2015-04-23 13:16:53 -0400

    Fixed update user. erip

--- VowelCat/settings.py
+++ VowelCat/settings.py
@@ -130,3 +130,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
